447a2fbf68e7c65465b0d4ced57011a161457fd5
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-07 21:16:29 +0100

    init

---
+++ src/main/java/com/durys/jakub/recruitmentapp/RecruitmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.durys.jakub.recruitmentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentAppApplication.class, args);
+	}
+
+}
