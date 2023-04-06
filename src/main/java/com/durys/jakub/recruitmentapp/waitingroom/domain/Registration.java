a310741abae4f648c0ce4c3ff4626d49ca576c07
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-06 16:40:43 +0200

    init of registration

---
+++ src/main/java/com/durys/jakub/recruitmentapp/waitingroom/domain/Registration.java
@@ -0,0 +1,10 @@
+package com.durys.jakub.recruitmentapp.waitingroom.domain;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Registration {
+
+    private final RegistrationId id;
+
+}
---
+++ src/main/java/com/durys/jakub/recruitmentapp/waitingroom/domain/RegistrationId.java
@@ -0,0 +1,6 @@
+package com.durys.jakub.recruitmentapp.waitingroom.domain;
+
+import java.util.UUID;
+
+public record RegistrationId(UUID value) {
+}
