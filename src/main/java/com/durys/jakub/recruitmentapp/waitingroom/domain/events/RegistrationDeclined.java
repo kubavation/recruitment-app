a7c1d0c377588f0d2f1fb615cbb84f9e0c9630f5
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-07 20:18:19 +0200

    offerId field in RegistrationDeclined event

--- src/main/java/com/durys/jakub/recruitmentapp/waitingroom/domain/events/RegistrationDeclined.java
+++ src/main/java/com/durys/jakub/recruitmentapp/waitingroom/domain/events/RegistrationDeclined.java
@@ -2,8 +2,9 @@ package com.durys.jakub.recruitmentapp.waitingroom.domain.events;
 
 import com.durys.jakub.recruitmentapp.ddd.annotations.events.DomainEvent;
 import com.durys.jakub.recruitmentapp.waitingroom.domain.ApplicantInformation;
+import com.durys.jakub.recruitmentapp.waitingroom.domain.OfferId;
 import com.durys.jakub.recruitmentapp.waitingroom.domain.RegistrationId;
 
-public record RegistrationDeclined(RegistrationId registrationId, ApplicantInformation applicantInformation,
-                                   String reason) implements DomainEvent {
+public record RegistrationDeclined(RegistrationId registrationId, OfferId offerId,
+                                   ApplicantInformation applicantInformation, String reason) implements DomainEvent {
 }
