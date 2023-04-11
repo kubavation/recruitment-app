e455a2055aebcaefe68bb4594ded64dffb854c94
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-11 20:29:51 +0200

    Opinion vo

---
+++ src/main/java/com/durys/jakub/recruitmentapp/cv/domain/Opinion.java
@@ -0,0 +1,9 @@
+package com.durys.jakub.recruitmentapp.cv.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+class Opinion {
+    private final ReviewerId reviewerId;
+    private final String text;
+}
---
+++ src/main/java/com/durys/jakub/recruitmentapp/cv/domain/ReviewerId.java
@@ -0,0 +1,5 @@
+package com.durys.jakub.recruitmentapp.cv.domain;
+
+import java.util.UUID;
+
+public record ReviewerId(UUID value) {}
