2d47e648a61991210fe944a7175601822d452e2a
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-11 19:36:08 +0200

    DomainEventRegistry

--- src/main/java/com/durys/jakub/recruitmentapp/ddd/annotations/events/DomainEventRegistry.java
+++ src/main/java/com/durys/jakub/recruitmentapp/ddd/annotations/events/DomainEventRegistry.java
@@ -1,14 +1,32 @@
 package com.durys.jakub.recruitmentapp.ddd.annotations.events;
 
-public class DomainEventRegistry {
+import com.durys.jakub.recruitmentapp.events.EventEmitter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
+
+@RequiredArgsConstructor
+@Component
+public class DomainEventRegistry implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
 
     public static DomainEventRegistry instance() {
         return new DomainEventRegistry();
     }
 
     public void publish(DomainEvent event) {
-        //todo emit
+        applicationContext
+                .getBean(EventEmitter.class)
+                .emit(event);
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        applicationContext = context;
+    }
 }
