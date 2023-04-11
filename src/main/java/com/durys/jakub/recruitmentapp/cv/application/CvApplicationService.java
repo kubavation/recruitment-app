70343429e10a5a5ac07884257017b248679d1f11
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-11 20:51:14 +0200

    onAcceptedRegistration method implementation

--- src/main/java/com/durys/jakub/recruitmentapp/cv/application/CvApplicationService.java
+++ src/main/java/com/durys/jakub/recruitmentapp/cv/application/CvApplicationService.java
@@ -1,15 +1,27 @@
 package com.durys.jakub.recruitmentapp.cv.application;
 
+import com.durys.jakub.recruitmentapp.cv.domain.Cv;
+import com.durys.jakub.recruitmentapp.cv.domain.CvId;
+import com.durys.jakub.recruitmentapp.cv.domain.CvRepository;
 import com.durys.jakub.recruitmentapp.ddd.annotations.ApplicationService;
 import com.durys.jakub.recruitmentapp.registration.domain.events.RegistrationAccepted;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 
+import java.util.UUID;
+
 @ApplicationService
+@RequiredArgsConstructor
 public class CvApplicationService {
 
+    private final CvRepository cvRepository;
 
     @EventListener
     public void onAcceptedRegistration(RegistrationAccepted event) {
-        //todo
+
+        Cv cv = new Cv(new CvId(UUID.randomUUID()), event.registrationId());
+        cvRepository.save(cv);
+
+        //todo notification for reviewers
     }
 }
