a28e5a4db239fb213376e387494c534e435b0318
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-11 20:13:39 +0200

    eventListener on cvApplicationService

--- src/main/java/com/durys/jakub/recruitmentapp/cv/application/CvApplicationService.java
+++ src/main/java/com/durys/jakub/recruitmentapp/cv/application/CvApplicationService.java
@@ -1,7 +1,15 @@
 package com.durys.jakub.recruitmentapp.cv.application;
 
 import com.durys.jakub.recruitmentapp.ddd.annotations.ApplicationService;
+import com.durys.jakub.recruitmentapp.registration.domain.events.RegistrationAccepted;
+import org.springframework.context.event.EventListener;
 
 @ApplicationService
 public class CvApplicationService {
+
+
+    @EventListener
+    public void onAcceptedRegistration(RegistrationAccepted event) {
+        //todo
+    }
 }
