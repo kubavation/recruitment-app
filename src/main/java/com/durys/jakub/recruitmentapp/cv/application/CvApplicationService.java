13a83fbc6e00742522a7a8ab99356052597f66ea
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-11 20:06:55 +0200

    init of cvApplicationService

---
+++ src/main/java/com/durys/jakub/recruitmentapp/cv/application/CvApplicationService.java
@@ -0,0 +1,7 @@
+package com.durys.jakub.recruitmentapp.cv.application;
+
+import com.durys.jakub.recruitmentapp.ddd.annotations.ApplicationService;
+
+@ApplicationService
+public class CvApplicationService {
+}
