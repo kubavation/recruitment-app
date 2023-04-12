ad6675988593e359c654c5e0852f7811fd01b9ba
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-12 19:17:03 +0200

    cvAccepted/declined events

---
+++ src/main/java/com/durys/jakub/recruitmentapp/cv/domain/events/CvAccepted.java
@@ -0,0 +1,8 @@
+package com.durys.jakub.recruitmentapp.cv.domain.events;
+
+import com.durys.jakub.recruitmentapp.cv.domain.CvId;
+import com.durys.jakub.recruitmentapp.ddd.annotations.events.DomainEvent;
+import com.durys.jakub.recruitmentapp.registration.domain.RegistrationId;
+
+public record CvAccepted(CvId cvId, RegistrationId registrationId) implements DomainEvent {
+}
