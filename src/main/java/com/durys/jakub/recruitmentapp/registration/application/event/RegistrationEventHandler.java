aa6a1425a7b2b13b794644866fe0ca4c40e89df8
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-13 20:30:12 +0200

    handlers for cv changes

--- src/main/java/com/durys/jakub/recruitmentapp/registration/application/event/RegistrationEventHandler.java
+++ src/main/java/com/durys/jakub/recruitmentapp/registration/application/event/RegistrationEventHandler.java
@@ -1,7 +1,21 @@
 package com.durys.jakub.recruitmentapp.registration.application.event;
 
+import com.durys.jakub.recruitmentapp.cv.domain.events.CvAccepted;
+import com.durys.jakub.recruitmentapp.cv.domain.events.CvDeclined;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RegistrationEventHandler {
+
+
+    @EventListener
+    public void onCvAccepted(CvAccepted event) {
+
+    }
+
+    @EventListener
+    public void onCvDeclined(CvDeclined event) {
+
+    }
 }
