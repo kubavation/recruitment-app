ca12404dee637cdd434d11213ef99989567d7d03
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-13 17:48:58 +0200

    constructors and activate/deactivate methods in offer

--- src/main/java/com/durys/jakub/recruitmentapp/offer/domain/Offer.java
+++ src/main/java/com/durys/jakub/recruitmentapp/offer/domain/Offer.java
@@ -1,8 +1,5 @@
 package com.durys.jakub.recruitmentapp.offer.domain;
 
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 public class Offer {
 
     public enum Status {
@@ -14,5 +11,31 @@ public class Offer {
     private final Description description;
     private final ApplicantLimit limit;
     private final OfferPeriod period;
+    private Status status;
 
+    public Offer(OfferId offerId, Position position, Description description, ApplicantLimit limit, OfferPeriod period) {
+        this.offerId = offerId;
+        this.position = position;
+        this.description = description;
+        this.limit = limit;
+        this.period = period;
+        this.status = Status.NEW;
+    }
+
+    public Offer(OfferId offerId, Position position, Description description, ApplicantLimit limit, OfferPeriod period, Status status) {
+        this.offerId = offerId;
+        this.position = position;
+        this.description = description;
+        this.limit = limit;
+        this.period = period;
+        this.status = status;
+    }
+
+    public void activate() {
+        this.status = Status.ACTIVE;
+    }
+
+    public void deactivate() {
+        this.status = Status.INACTIVE;
+    }
 }
