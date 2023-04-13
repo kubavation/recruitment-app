853b64d389a22ba1b1841da88831067e619478fa
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-13 17:20:04 +0200

    RegistrationExceptionType

---
+++ src/main/java/com/durys/jakub/recruitmentapp/registration/domain/exceptions/RegistrationExceptionType.java
@@ -0,0 +1,6 @@
+package com.durys.jakub.recruitmentapp.registration.domain.exceptions;
+
+public enum RegistrationExceptionType {
+    INVALID_STATUS_FOR_REJECTION,
+    INVALID_STATUS_FOR_ACCEPTATION
+}
