eb9221a91cd33b52674709b85298b5743e11b8d9
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-13 18:42:33 +0200

    addOffer in applicationService

--- src/main/java/com/durys/jakub/recruitmentapp/offer/application/OfferApplicationService.java
+++ src/main/java/com/durys/jakub/recruitmentapp/offer/application/OfferApplicationService.java
@@ -1,14 +1,32 @@
 package com.durys.jakub.recruitmentapp.offer.application;
 
 import com.durys.jakub.recruitmentapp.ddd.annotations.ApplicationService;
-import com.durys.jakub.recruitmentapp.offer.domain.OfferRepository;
+import com.durys.jakub.recruitmentapp.offer.domain.*;
+import com.durys.jakub.recruitmentapp.sharedkernel.identity.DomainIdentityProvider;
+import com.durys.jakub.recruitmentapp.sharedkernel.identity.UUIDDomainIdentityProvider;
 import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 @ApplicationService
 @RequiredArgsConstructor
 public class OfferApplicationService {
 
     private final OfferRepository offerRepository;
+    private final DomainIdentityProvider<UUID> identityProvider = new UUIDDomainIdentityProvider();
+
+    public void addOffer(String position, String description, Integer applicantLimit, LocalDate from, LocalDate to) {
+
+        Offer offer = new Offer(
+                new OfferId(identityProvider.identity()),
+                new Position(position),
+                new Description(description),
+                new ApplicantLimit(applicantLimit),
+                new OfferPeriod(from, to));
+
+        offerRepository.save(offer);
+    }
 
 
 }
