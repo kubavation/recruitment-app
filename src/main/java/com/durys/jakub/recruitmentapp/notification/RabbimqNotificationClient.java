d6ef61be430a2f8746cae9e7e1ef9417dbb9c0d9
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-17 20:07:39 +0200

    RabbimqNotificationClient

---
+++ src/main/java/com/durys/jakub/recruitmentapp/notification/RabbimqNotificationClient.java
@@ -0,0 +1,21 @@
+package com.durys.jakub.recruitmentapp.notification;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RabbimqNotificationClient implements NotificationClient {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${queue.notification}")
+    private String notificationQueue;
+
+    @Override
+    public void publish(Notification notification) {
+        rabbitTemplate.convertAndSend(notificationQueue, "", notification);
+    }
+}
