ef08f469455fab99b3baa19f3f064256a35ec8a7
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-17 19:22:29 +0200

    mail dto + intefrace for mailClient

---
+++ src/main/java/com/durys/jakub/recruitmentapp/mail/Mail.java
@@ -0,0 +1,3 @@
+package com.durys.jakub.recruitmentapp.mail;
+
+public record Mail(String destination, String title, String content) { }
---
+++ src/main/java/com/durys/jakub/recruitmentapp/mail/MailClient.java
@@ -0,0 +1,5 @@
+package com.durys.jakub.recruitmentapp.mail;
+
+public interface MailClient {
+    void send(Mail mail);
+}
