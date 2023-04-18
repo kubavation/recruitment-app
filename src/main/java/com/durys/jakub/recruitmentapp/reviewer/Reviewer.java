e3c2b0fbb7fc9d269169cf4bd1f2c51e6880eb4c
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-18 16:55:24 +0200

    reviewer/reviewerId - reviewer domain

---
+++ src/main/java/com/durys/jakub/recruitmentapp/reviewer/Reviewer.java
@@ -0,0 +1,9 @@
+package com.durys.jakub.recruitmentapp.reviewer;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Reviewer {
+
+    private final ReviewerId reviewerId;
+}
---
+++ src/main/java/com/durys/jakub/recruitmentapp/reviewer/ReviewerId.java
@@ -0,0 +1,6 @@
+package com.durys.jakub.recruitmentapp.reviewer;
+
+import java.util.UUID;
+
+public record ReviewerId(UUID tenantId) {
+}
