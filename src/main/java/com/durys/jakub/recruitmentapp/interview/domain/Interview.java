ecc5ca0c9adf4179b640299d35be12592dbaeeb0
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-18 20:57:36 +0200

    InterviewAcceptanceLevel enum

--- src/main/java/com/durys/jakub/recruitmentapp/interview/domain/Interview.java
+++ src/main/java/com/durys/jakub/recruitmentapp/interview/domain/Interview.java
@@ -1,10 +1,12 @@
 package com.durys.jakub.recruitmentapp.interview.domain;
 
+import com.durys.jakub.recruitmentapp.registration.domain.RegistrationId;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class Interview {
 
     private final InterviewId interviewId;
+    private final RegistrationId registrationId;
 
 }
---
+++ src/main/java/com/durys/jakub/recruitmentapp/interview/domain/InterviewAcceptanceLevel.java
@@ -0,0 +1,6 @@
+package com.durys.jakub.recruitmentapp.interview.domain;
+
+public enum InterviewAcceptanceLevel {
+    THREE_ACCEPTATIONS,
+    TWO_ACCEPTATIONS;
+}
