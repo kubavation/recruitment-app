e6ccc77ec629a4b6f170d8fec5be82173ff7efb2
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-18 19:39:13 +0200

    import clear

--- src/main/java/com/durys/jakub/recruitmentapp/reviewer/infrastructure/AMReviewerRepository.java
+++ src/main/java/com/durys/jakub/recruitmentapp/reviewer/infrastructure/AMReviewerRepository.java
@@ -8,10 +8,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Optional;
-
 @RequiredArgsConstructor
 public class AMReviewerRepository implements ReviewerRepository {
 
