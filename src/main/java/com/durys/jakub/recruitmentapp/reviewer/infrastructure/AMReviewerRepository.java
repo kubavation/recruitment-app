dae1d52d418214845da42eaf0176adca21930342
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-18 18:04:49 +0200

    loadbalaned webClient + changes in amReviewerRepo config

--- src/main/java/com/durys/jakub/recruitmentapp/reviewer/infrastructure/AMReviewerRepository.java
+++ src/main/java/com/durys/jakub/recruitmentapp/reviewer/infrastructure/AMReviewerRepository.java
@@ -3,12 +3,17 @@ package com.durys.jakub.recruitmentapp.reviewer.infrastructure;
 import com.durys.jakub.recruitmentapp.reviewer.domain.Reviewer;
 import com.durys.jakub.recruitmentapp.reviewer.domain.ReviewerId;
 import com.durys.jakub.recruitmentapp.reviewer.domain.ReviewerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
 
+@RequiredArgsConstructor
 public class AMReviewerRepository implements ReviewerRepository {
+
+    private final WebClient webClient;
 
     @Override
     public Collection<Reviewer> load() {
