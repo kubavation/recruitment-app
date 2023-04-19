52b1e7bed1616ce9bf07b2f22aaada0d2174516b
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-19 19:18:10 +0200

    Reviewer domain -> leavePositiveOpinion/NegativeOpinion methods

--- src/main/java/com/durys/jakub/recruitmentapp/reviewer/domain/Reviewer.java
+++ src/main/java/com/durys/jakub/recruitmentapp/reviewer/domain/Reviewer.java
@@ -1,3 +1,15 @@
 package com.durys.jakub.recruitmentapp.reviewer.domain;
 
-public record Reviewer(ReviewerId reviewerId, String name, String email) { }
+import com.durys.jakub.recruitmentapp.technicalinterview.domain.TechnicalInterview;
+
+public record Reviewer(ReviewerId reviewerId, String name, String email) {
+
+    public void leavePositiveOpinion(TechnicalInterview interview, String opinion) {
+        interview.accept(reviewerId, opinion);
+    }
+
+    public void leaveNegativeOpinion(TechnicalInterview interview, String opinion) {
+        interview.decline(reviewerId, opinion);
+    }
+
+}
