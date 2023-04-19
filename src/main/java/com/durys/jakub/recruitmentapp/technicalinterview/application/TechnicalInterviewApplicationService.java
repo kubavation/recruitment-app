7729625f328d4bb4b97fd7e13bcf79d95b4dc032
kubavation <37585627+kubavation@users.noreply.github.com>
2023-04-19 19:32:26 +0200

    addPositiveOpinion in TechnicalInterviewApplicationService

--- src/main/java/com/durys/jakub/recruitmentapp/technicalinterview/application/TechnicalInterviewApplicationService.java
+++ src/main/java/com/durys/jakub/recruitmentapp/technicalinterview/application/TechnicalInterviewApplicationService.java
@@ -1,13 +1,30 @@
 package com.durys.jakub.recruitmentapp.technicalinterview.application;
 
 import com.durys.jakub.recruitmentapp.ddd.annotations.ApplicationService;
+import com.durys.jakub.recruitmentapp.reviewer.domain.ReviewerId;
+import com.durys.jakub.recruitmentapp.reviewer.domain.ReviewerRepository;
+import com.durys.jakub.recruitmentapp.technicalinterview.domain.TechnicalInterview;
+import com.durys.jakub.recruitmentapp.technicalinterview.domain.TechnicalInterviewId;
 import com.durys.jakub.recruitmentapp.technicalinterview.domain.TechnicalInterviewRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
 
 @ApplicationService
 @RequiredArgsConstructor
 public class TechnicalInterviewApplicationService {
 
     private final TechnicalInterviewRepository interviewRepository;
+    private final ReviewerRepository reviewerRepository;
+
+    public void addPositiveOpinion(UUID interviewId, UUID reviewerId, String opinion) {
+
+        TechnicalInterview technicalInterview = interviewRepository.load(new TechnicalInterviewId(interviewId))
+                .orElseThrow(RuntimeException::new); //todo
+
+        reviewerRepository.load(new ReviewerId(reviewerId))
+                .subscribe(reviewer -> reviewer.leavePositiveOpinion(technicalInterview, opinion));
+
+    }
 
 }
