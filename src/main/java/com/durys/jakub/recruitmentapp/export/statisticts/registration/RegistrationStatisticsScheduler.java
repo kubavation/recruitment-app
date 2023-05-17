19bed7fc05b0a283152de0657701e76bbfe96b52
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-17 17:36:01 +0200

    RegistrationStatisticsScheduler - properties + conditionally creating based on application.yml

--- src/main/java/com/durys/jakub/recruitmentapp/export/statisticts/registration/RegistrationStatisticsScheduler.java
+++ src/main/java/com/durys/jakub/recruitmentapp/export/statisticts/registration/RegistrationStatisticsScheduler.java
@@ -1,4 +1,19 @@
 package com.durys.jakub.recruitmentapp.export.statisticts.registration;
 
-public class RegistrationStatisticsScheduler {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@ConditionalOnExpression("${export.registration-statistics.enabled:false}")
+class RegistrationStatisticsScheduler {
+
+
+    @Scheduled(cron = "${export.registration-statistics.cron}")
+    void export() {
+        log.info("registration statistics export - start");
+    }
+
 }
