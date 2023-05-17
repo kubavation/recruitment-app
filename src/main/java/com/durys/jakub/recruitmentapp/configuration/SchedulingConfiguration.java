feca4e1a9b4ca6611fad761fecc77b14ccbb4ac3
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-17 17:24:59 +0200

    configuration of taskScheduler

--- src/main/java/com/durys/jakub/recruitmentapp/configuration/SchedulingConfiguration.java
+++ src/main/java/com/durys/jakub/recruitmentapp/configuration/SchedulingConfiguration.java
@@ -1,11 +1,22 @@
 package com.durys.jakub.recruitmentapp.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 @EnableScheduling
 @EnableAsync
 class SchedulingConfiguration {
+
+    @Bean
+    TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(5);
+        threadPoolTaskScheduler.setThreadNamePrefix("ThreadPoolTaskScheduler");
+        return threadPoolTaskScheduler;
+    }
 }
