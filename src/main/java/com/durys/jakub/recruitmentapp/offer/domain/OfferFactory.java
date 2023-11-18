f0832b393581f23f9bd5bfd180f7dd88d51b89f2
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-18 23:45:47 +0100

    init JdbcOfferRepository

--- src/main/java/com/durys/jakub/recruitmentapp/offer/domain/OfferFactory.java
+++ src/main/java/com/durys/jakub/recruitmentapp/offer/domain/OfferFactory.java
@@ -1,7 +1,12 @@
 package com.durys.jakub.recruitmentapp.offer.domain;
 
-import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class OfferFactory {
 
     public static Offer create(String position, String description, Integer applicantLimit, LocalDate from, LocalDate to) {
@@ -10,6 +15,18 @@ public class OfferFactory {
                 new Description(description),
                 new ApplicantLimit(applicantLimit),
                 new OfferPeriod(from, to)
+        );
+    }
+
+    public static Offer create(UUID id, String position, String description, Integer applicantLimit,
+                               LocalDate from, LocalDate to, String state) {
+        return new Offer(
+                new Offer.Id(id),
+                new Position(position),
+                new Description(description),
+                new ApplicantLimit(applicantLimit),
+                new OfferPeriod(from, to),
+                Offer.Status.valueOf(state)
         );
     }
 
