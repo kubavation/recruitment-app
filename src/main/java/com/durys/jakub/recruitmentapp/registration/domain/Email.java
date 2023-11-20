9c622e148f2db9ef94246e59db6dc0ece04d5c1c
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-20 20:52:49 +0100

    email vo

---
+++ src/main/java/com/durys/jakub/recruitmentapp/registration/domain/Email.java
@@ -0,0 +1,17 @@
+package com.durys.jakub.recruitmentapp.registration.domain;
+
+import com.durys.jakub.recruitmentapp.commons.exception.ValidationException;
+import org.apache.commons.lang.StringUtils;
+
+record Email(String value) {
+
+
+    static void test(String email) {
+
+        if (StringUtils.isEmpty(email)) {
+            throw new ValidationException("Email cannot be empty");
+        }
+
+        //todo additional validation
+    }
+}
--- src/main/java/com/durys/jakub/recruitmentapp/registration/domain/exceptions/RegistrationExceptionType.java
+++
@@ -1,6 +0,0 @@
-package com.durys.jakub.recruitmentapp.registration.domain.exceptions;
-
-public enum RegistrationExceptionType {
-    INVALID_STATUS_FOR_REJECTION,
-    INVALID_STATUS_FOR_ACCEPTATION
-}
