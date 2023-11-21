86178eff5d503137b9ec9191c4643342af7e0956
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-21 20:19:30 +0100

    handling RejectRegistrationCommand

--- src/test/java/com/durys/jakub/recruitmentapp/registration/application/RegistrationApplicationServiceTest.java
+++ src/test/java/com/durys/jakub/recruitmentapp/registration/application/RegistrationApplicationServiceTest.java
@@ -1,15 +1,16 @@
 package com.durys.jakub.recruitmentapp.registration.application;
 
 import com.durys.jakub.recruitmentapp.registration.domain.Registration;
+import com.durys.jakub.recruitmentapp.registration.domain.RegistrationFactory;
 import com.durys.jakub.recruitmentapp.registration.domain.RegistrationRepository;
+import com.durys.jakub.recruitmentapp.registration.domain.command.RejectRegistrationCommand;
 import com.durys.jakub.recruitmentapp.registration.domain.command.SubmitRegistrationCommand;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.UUID;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class RegistrationApplicationServiceTest {
 
@@ -23,6 +24,22 @@ class RegistrationApplicationServiceTest {
 
         var command = new SubmitRegistrationCommand(UUID.randomUUID(), "John", "Doe",
                 "johndoe@gmail.com", "333222333", "cv.pdf", new byte[] {});
+
+        service.handle(command);
+
+        verify(registrationRepository).save(Mockito.any(Registration.class));
+    }
+
+    @Test
+    void shouldRejectRegistration() {
+
+        var registrationId = new Registration.Id(UUID.randomUUID());
+        var command = new RejectRegistrationCommand(registrationId.value(), "Reason");
+
+        when(registrationRepository.load(registrationId)).thenReturn(
+                RegistrationFactory.create(
+                    UUID.randomUUID(), "John", "Doe", "jondoe@gmail.com",
+                    "430212343", "cv.pdf", new byte[]{}));
 
         service.handle(command);
 
