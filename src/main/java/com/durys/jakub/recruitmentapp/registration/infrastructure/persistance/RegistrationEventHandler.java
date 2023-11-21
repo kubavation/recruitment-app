a3c6678c8ba09cd4dddb256217945a241d44b34f
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-21 21:01:54 +0100

    initial RegistrationEventHandler

---
+++ src/main/java/com/durys/jakub/recruitmentapp/registration/infrastructure/persistance/RegistrationEventHandler.java
@@ -0,0 +1,30 @@
+package com.durys.jakub.recruitmentapp.registration.infrastructure.persistance;
+
+import com.durys.jakub.recruitmentapp.events.EventHandler;
+import com.durys.jakub.recruitmentapp.registration.domain.events.RegistrationEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import static com.durys.jakub.recruitmentapp.registration.domain.events.RegistrationEvent.*;
+
+@Component
+@Slf4j
+class RegistrationEventHandler implements EventHandler<RegistrationEvent> {
+
+    @Override
+    public void handle(RegistrationEvent registrationEvent) {
+        switch (registrationEvent) {
+            case RegistrationApproved approved -> handle(approved);
+            case RegistrationRejected rejected -> handle(rejected);
+            case default -> log.warn("Unsupported event {}", registrationEvent);
+        }
+    }
+
+    void handle(RegistrationApproved event) {
+
+    }
+
+    void handle(RegistrationRejected event) {
+
+    }
+}
