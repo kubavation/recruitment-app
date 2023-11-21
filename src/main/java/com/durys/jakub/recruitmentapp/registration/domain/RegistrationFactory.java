53c52af53942cb1b18440c3162dfa7926eb0369f
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-21 19:38:59 +0100

    additional factory method

--- src/main/java/com/durys/jakub/recruitmentapp/registration/domain/RegistrationFactory.java
+++ src/main/java/com/durys/jakub/recruitmentapp/registration/domain/RegistrationFactory.java
@@ -17,4 +17,18 @@ public class RegistrationFactory {
         );
     }
 
+    public static Registration create(UUID id, UUID offerId, String applicantFirstName, String applicantLastName,
+                                      String email, String phoneNumber, String fileName,
+                                      byte[] file, String rejectionReason, String state) {
+
+        return new Registration(
+                new Registration.Id(id),
+                new Offer.Id(offerId),
+                new ApplicantInformation(applicantFirstName, applicantLastName, email, phoneNumber),
+                new Cv(fileName, file),
+                rejectionReason.isEmpty() ? null : new RejectionReason(rejectionReason),
+                Registration.Status.valueOf(state)
+        );
+    }
+
 }
