8372e2aaab662011f7713c488ef4b073e0d314eb
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-22 22:02:14 +0100

    additional tests

--- src/test/java/com/durys/jakub/recruitmentapp/registration/infrastructure/persistance/RegistrationEventHandlerTest.java
+++ src/test/java/com/durys/jakub/recruitmentapp/registration/infrastructure/persistance/RegistrationEventHandlerTest.java
@@ -2,6 +2,7 @@ package com.durys.jakub.recruitmentapp.registration.infrastructure.persistance;
 
 import com.durys.jakub.recruitmentapp.offer.domain.Offer;
 import com.durys.jakub.recruitmentapp.offer.infrastructure.persistance.OfferEntity;
+import com.durys.jakub.recruitmentapp.registration.domain.Registration;
 import com.durys.jakub.recruitmentapp.registration.domain.events.RegistrationEvent;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
@@ -13,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
@@ -43,6 +45,7 @@ class RegistrationEventHandlerTest {
     @Test
     @Transactional
     void shouldCreateRegistrationEntity() {
+
         var id = UUID.randomUUID();
         init();
 
@@ -53,6 +56,34 @@ class RegistrationEventHandlerTest {
         registrationEventHandler.handle(event);
 
         assertNotNull(entityManager.find(RegistrationEntity.class, id));
+    }
+
+    @Test
+    @Transactional
+    void shouldApproveRegistrationEntity() {
+
+        UUID registrationId = addRegistration();
+
+        var event = new RegistrationEvent.RegistrationApproved(registrationId);
+
+        registrationEventHandler.handle(event);
+
+        assertThat(entityManager.find(RegistrationEntity.class, registrationId).getStatus()).isEqualTo(Registration.Status.Approved.name());
+    }
+
+    private UUID addRegistration() {
+
+        OfferEntity offer = entityManager.find(OfferEntity.class, offerId);
+
+        RegistrationEntity registration = new RegistrationEntity(
+                UUID.randomUUID(), offer,
+                new ApplicantInformation("John", "Doe","johndoe@gmail.com", "321321211"),
+                "cv.pdf", null, new byte[] {},
+                Registration.Status.Submitted.name());
+
+        entityManager.persist(registration);
+        entityManager.flush();
+        return registration.getId();
     }
 
 }
