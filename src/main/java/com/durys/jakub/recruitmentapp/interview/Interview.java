ec86abf158c15b537c7c337f8f169440077bc652
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-26 19:20:58 +0100

    additional fields in Interview

--- src/main/java/com/durys/jakub/recruitmentapp/interview/Interview.java
+++ src/main/java/com/durys/jakub/recruitmentapp/interview/Interview.java
@@ -1,5 +1,11 @@
 package com.durys.jakub.recruitmentapp.interview;
 
+import com.durys.jakub.recruitmentapp.offer.domain.Offer;
+import com.durys.jakub.recruitmentapp.registration.domain.Registration;
+import com.durys.jakub.recruitmentapp.sharedkernel.ReviewerId;
+import com.durys.jakub.recruitmentapp.sharedkernel.TenantId;
+
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class Interview {
@@ -8,5 +14,10 @@ public class Interview {
     }
 
     private final Id id;
+    private final LocalDateTime at;
+    private final Registration.Id registrationId;
+    private final Offer.Id offerId;
+    private final TenantId tenantId;
+    private ReviewerId reviewerId;
 
 }
