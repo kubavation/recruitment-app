85a49be74cc24e9b1cf62564bfc5a59a151cd7c9
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-26 19:29:56 +0100

    init interviewEvent

--- src/main/java/com/durys/jakub/recruitmentapp/interview/Interview.java
+++ src/main/java/com/durys/jakub/recruitmentapp/interview/Interview.java
@@ -15,10 +15,25 @@ public class Interview extends AggregateRoot {
     }
 
     private final Id id;
-    private final LocalDateTime at;
+    private LocalDateTime at;
     private final Registration.Id registrationId;
     private final Offer.Id offerId;
     private final TenantId tenantId;
     private ReviewerId reviewerId;
+
+    public Interview(Id id, LocalDateTime at, Registration.Id registrationId, Offer.Id offerId, TenantId tenantId) {
+        this.id = id;
+        this.at = at;
+        this.registrationId = registrationId;
+        this.offerId = offerId;
+        this.tenantId = tenantId;
+    }
+
+    public void assignReviewer(ReviewerId reviewerId) {
+        //todo
+        this.reviewerId = reviewerId;
+    }
+
+
 
 }
