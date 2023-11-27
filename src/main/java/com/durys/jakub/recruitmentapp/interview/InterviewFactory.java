e7558175b6b8e670247dc5fc61f8bf8b8c2ae046
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-27 18:48:55 +0100

    additional factory method

--- src/main/java/com/durys/jakub/recruitmentapp/interview/InterviewFactory.java
+++ src/main/java/com/durys/jakub/recruitmentapp/interview/InterviewFactory.java
@@ -2,14 +2,29 @@ package com.durys.jakub.recruitmentapp.interview;
 
 import com.durys.jakub.recruitmentapp.offer.domain.Offer;
 import com.durys.jakub.recruitmentapp.registration.domain.Registration;
+import com.durys.jakub.recruitmentapp.sharedkernel.ReviewerId;
 import com.durys.jakub.recruitmentapp.sharedkernel.TenantId;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class InterviewFactory {
 
     public static Interview create(UUID registrationId, UUID offerId, TenantId tenantId) {
         return new Interview(new Registration.Id(registrationId), new Offer.Id(offerId), tenantId);
+    }
+
+    public static Interview create(UUID id, LocalDateTime at, UUID registrationId, UUID offerId,
+              TenantId tenantId, UUID reviewerId, String state) {
+        
+        return new Interview(
+                new Interview.Id(id),
+                at,
+                new Registration.Id(registrationId),
+                new Offer.Id(offerId),
+                tenantId,
+                new ReviewerId(UUID.randomUUID()),
+                Interview.State.valueOf(state));
     }
 
 }
