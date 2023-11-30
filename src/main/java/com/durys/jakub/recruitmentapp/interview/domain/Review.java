3aad1c16c1c48e1f66d237d32d20dd9e3d9fa829
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-30 20:09:32 +0100

    changes in interview model

--- src/main/java/com/durys/jakub/recruitmentapp/interview/domain/Review.java
+++ src/main/java/com/durys/jakub/recruitmentapp/interview/domain/Review.java
@@ -1,41 +1,13 @@
 package com.durys.jakub.recruitmentapp.interview.domain;
 
-import com.durys.jakub.recruitmentapp.sharedkernel.ReviewerId;
-
-import java.time.LocalDateTime;
-
 class Review {
 
-    enum State {
-        Waiting, Accepted, Declined, Completed
-    }
+    //additional info
+    String opinion;
+    boolean acceptation;
 
-    private ReviewerId reviewerId;
-    private LocalDateTime at;
-    private Reply reply;
-    private State state;
-
-    Review(ReviewerId reviewerId, LocalDateTime at) {
-        this.reviewerId = reviewerId;
-        this.at = at;
-        this.state = State.Waiting;
-    }
-
-    void acceptInvitation() {
-        state = State.Accepted;
-    }
-
-    void declineInvitation() {
-        reviewerId = null;
-        state = State.Declined;
-    }
-
-    void complete(String opinion, boolean acceptation) {
-       reply = new Reply(opinion, acceptation);
-       state = State.Completed;
-    }
-
-    ReviewerId reviewerId() {
-        return reviewerId;
+    Review(String opinion, boolean acceptation) {
+        this.opinion = opinion;
+        this.acceptation = acceptation;
     }
 }
---
+++ src/main/java/com/durys/jakub/recruitmentapp/interview/domain/Term.java
@@ -0,0 +1,6 @@
+package com.durys.jakub.recruitmentapp.interview.domain;
+
+import java.time.LocalDateTime;
+
+record Term(LocalDateTime value) {
+}
