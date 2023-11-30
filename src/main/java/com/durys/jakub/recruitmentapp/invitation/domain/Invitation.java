4422244e9169451c04e42403a89f822891d5699b
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-30 21:20:30 +0100

    additional methods - invitation

--- src/main/java/com/durys/jakub/recruitmentapp/invitation/domain/Invitation.java
+++ src/main/java/com/durys/jakub/recruitmentapp/invitation/domain/Invitation.java
@@ -1,9 +1,12 @@
 package com.durys.jakub.recruitmentapp.invitation.domain;
 
+import com.durys.jakub.recruitmentapp.commons.exception.InvalidStateForOperationException;
+import com.durys.jakub.recruitmentapp.commons.exception.ValidationException;
 import com.durys.jakub.recruitmentapp.ddd.AggregateRoot;
 import com.durys.jakub.recruitmentapp.interview.domain.Interview;
 import com.durys.jakub.recruitmentapp.sharedkernel.ReviewerId;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class Invitation extends AggregateRoot {
@@ -11,7 +14,7 @@ public class Invitation extends AggregateRoot {
     record Id(UUID value) {}
 
     enum State {
-        New, Accepted, Declined
+        New, Accepted, Declined, Closed
     }
 
     private final Id id;
@@ -28,5 +31,24 @@ public class Invitation extends AggregateRoot {
         this.reviewerId = reviewerId;
         this.availableTerms = availableTerms;
         this.state = state;
+    }
+
+    public Invitation(Interview.Id interviewId, ReviewerId reviewerId, AvailableTerms availableTerms) {
+        this.id = new Id(UUID.randomUUID());
+        this.interviewId = interviewId;
+        this.reviewerId = reviewerId;
+        this.availableTerms = availableTerms;
+        this.state = State.New;
+    }
+
+    public void changeTerm(LocalDateTime at) {
+
+        var term = new Term(at);
+
+        if (!availableTerms.dateValidWithAvailableTerms(term)) {
+            throw new ValidationException("Chosen date not in range of available terms");
+        }
+
+        this.interviewTerm = term;
     }
 }
