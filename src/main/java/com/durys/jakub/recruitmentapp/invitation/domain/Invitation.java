249f672fa4746d19c13c245d34650aab571e1e3e
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-30 21:04:46 +0100

    invitation vo + changes in aggregate

--- src/main/java/com/durys/jakub/recruitmentapp/invitation/domain/Invitation.java
+++ src/main/java/com/durys/jakub/recruitmentapp/invitation/domain/Invitation.java
@@ -1,6 +1,8 @@
 package com.durys.jakub.recruitmentapp.invitation.domain;
 
 import com.durys.jakub.recruitmentapp.ddd.AggregateRoot;
+import com.durys.jakub.recruitmentapp.interview.domain.Interview;
+import com.durys.jakub.recruitmentapp.sharedkernel.ReviewerId;
 
 import java.util.UUID;
 
@@ -8,5 +10,23 @@ public class Invitation extends AggregateRoot {
 
     record Id(UUID value) {}
 
+    enum State {
+        New, Accepted, Declined
+    }
 
+    private final Id id;
+    private final Interview.Id interviewId;
+    private final ReviewerId reviewerId;
+    private final AvailableTerms availableTerms;
+
+    private Term interviewTerm;
+    private State state;
+
+    Invitation(Id id, Interview.Id interviewId, ReviewerId reviewerId, AvailableTerms availableTerms, State state) {
+        this.id = id;
+        this.interviewId = interviewId;
+        this.reviewerId = reviewerId;
+        this.availableTerms = availableTerms;
+        this.state = state;
+    }
 }
---
+++ src/main/java/com/durys/jakub/recruitmentapp/invitation/domain/Term.java
@@ -0,0 +1,6 @@
+package com.durys.jakub.recruitmentapp.invitation.domain;
+
+import java.time.LocalDateTime;
+
+record Term(LocalDateTime value) {
+}
