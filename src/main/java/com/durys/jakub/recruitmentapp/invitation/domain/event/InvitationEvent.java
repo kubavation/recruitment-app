ba1870bc8bb8d5c3265255d695c247724e7658bf
kubavation <37585627+kubavation@users.noreply.github.com>
2023-11-30 21:40:59 +0100

    invitation events

--- src/main/java/com/durys/jakub/recruitmentapp/invitation/domain/event/InvitationEvent.java
+++ src/main/java/com/durys/jakub/recruitmentapp/invitation/domain/event/InvitationEvent.java
@@ -4,6 +4,7 @@ import com.durys.jakub.recruitmentapp.events.DomainEvent;
 import com.durys.jakub.recruitmentapp.sharedkernel.AvailableTerm;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,6 +15,20 @@ public sealed interface InvitationEvent extends DomainEvent {
 
         public InvitationReceived(UUID interviewId, UUID invitationId, UUID reviewerId, List<AvailableTerm> availableTerms) {
             this(UUID.randomUUID(), Instant.now(), interviewId, invitationId, reviewerId, availableTerms);
+        }
+    }
+
+    record InvitationAccepted(UUID id, Instant at, UUID interviewId, UUID invitationId, LocalDateTime term, UUID reviewerId) implements InvitationEvent {
+
+        public InvitationAccepted(UUID interviewId, UUID invitationId, LocalDateTime term, UUID reviewerId) {
+            this(UUID.randomUUID(), Instant.now(), interviewId, invitationId, term, reviewerId);
+        }
+    }
+
+    record InvitationDeclined(UUID id, Instant at, UUID interviewId, UUID invitationId, UUID reviewerId) implements InvitationEvent {
+
+        public InvitationDeclined(UUID interviewId, UUID invitationId, UUID reviewerId) {
+            this(UUID.randomUUID(), Instant.now(), interviewId, invitationId, reviewerId);
         }
     }
 }
