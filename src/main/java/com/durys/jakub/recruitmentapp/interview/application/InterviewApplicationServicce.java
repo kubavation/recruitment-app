7c2c8f1b548f1789ad0934360000903dd7654b3b
kubavation <37585627+kubavation@users.noreply.github.com>
2023-12-02 11:40:14 +0100

    InterviewApplicationServicce

---
+++ src/main/java/com/durys/jakub/recruitmentapp/interview/application/InterviewApplicationServicce.java
@@ -0,0 +1,47 @@
+package com.durys.jakub.recruitmentapp.interview.application;
+
+import com.durys.jakub.recruitmentapp.interview.domain.Interview;
+import com.durys.jakub.recruitmentapp.interview.domain.InterviewRepository;
+import com.durys.jakub.recruitmentapp.interview.domain.command.InterviewCommand;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InterviewApplicationServicce {
+
+    private final InterviewRepository interviewRepository;
+
+    InterviewApplicationServicce(InterviewRepository interviewRepository) {
+        this.interviewRepository = interviewRepository;
+    }
+
+    @Transactional
+    public void handle(InterviewCommand.AssignReviewerCommand command) {
+
+        Interview interview = interviewRepository.load(new Interview.Id(command.interviewId()));
+
+        interview.assignReviewer(command.reviewerId(), command.at());
+
+        interviewRepository.save(interview);
+    }
+
+    @Transactional
+    public void handle(InterviewCommand.CompleteInterviewCommand command) {
+
+        Interview interview = interviewRepository.load(new Interview.Id(command.interviewId()));
+
+        interview.complete(command.opinion(), command.acceptation());
+
+        interviewRepository.save(interview);
+    }
+
+    @Transactional
+    public void handle(InterviewCommand.SendInterviewCommand command) {
+
+        Interview interview = interviewRepository.load(new Interview.Id(command.interviewId()));
+
+        interview.sendInvitationTo(command.reviewerId());
+
+        interviewRepository.save(interview);
+    }
+}
