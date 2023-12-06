1da4bac3921fe3c70d6389f6f0b3f2b0f8b963d0
kubavation <37585627+kubavation@users.noreply.github.com>
2023-12-06 18:31:25 +0100

    assignReviewer endpoint

--- src/main/java/com/durys/jakub/recruitmentapp/interview/infrastructure/in/InterviewController.java
+++ src/main/java/com/durys/jakub/recruitmentapp/interview/infrastructure/in/InterviewController.java
@@ -4,6 +4,7 @@ import com.durys.jakub.recruitmentapp.interview.application.InterviewApplication
 import com.durys.jakub.recruitmentapp.interview.domain.command.InterviewCommand;
 import com.durys.jakub.recruitmentapp.invitation.application.InvitationApplicationService;
 import com.durys.jakub.recruitmentapp.invitation.domain.command.InvitationCommand;
+import com.durys.jakub.recruitmentapp.sharedkernel.ReviewerId;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -23,6 +24,13 @@ class InterviewController {
     void complete(@PathVariable UUID interviewId, @RequestBody CompleteInterviewDTO dto) {
         interviewApplicationService.handle(
             new InterviewCommand.CompleteInterviewCommand(interviewId, dto.opinion(), dto.acceptation())
+        );
+    }
+
+    @PatchMapping("/{interviewId}/reviewers")
+    void assignReviewer(@PathVariable UUID interviewId, @RequestParam UUID reviewerId, @RequestParam LocalDateTime at) {
+        interviewApplicationService.handle(
+                new InterviewCommand.AssignReviewerCommand(interviewId, new ReviewerId(reviewerId), at)
         );
     }
 }
