3759e6662dcd8001c9d58e57efef3d274b3b4402
kubavation <37585627+kubavation@users.noreply.github.com>
2023-12-07 19:33:33 +0100

    reviewer activate/ deactivate methods

--- src/main/java/com/durys/jakub/recruitmentapp/reviewer/Reviewer.java
+++ src/main/java/com/durys/jakub/recruitmentapp/reviewer/Reviewer.java
@@ -1,13 +1,46 @@
 package com.durys.jakub.recruitmentapp.reviewer;
 
 
+import com.durys.jakub.recruitmentapp.commons.exception.InvalidStateForOperationException;
+
 public class Reviewer {
+
+    public enum State {
+        ACTIVE, ARCHIVED
+    }
 
     private final ReviewerId id;
     private final String name;
+    private State state;
+
 
     public Reviewer(ReviewerId id, String name) {
         this.id = id;
         this.name = name;
+        activate();
+    }
+
+    public Reviewer(ReviewerId id, String name, State state) {
+        this.id = id;
+        this.name = name;
+        this.state = state;
+    }
+
+    public void activate() {
+
+        if (state == State.ACTIVE) {
+            throw new InvalidStateForOperationException("Cannot activate reviewer");
+        }
+
+        this.state = State.ACTIVE;
+    }
+
+    public void archive() {
+
+        if (state == State.ARCHIVED) {
+            throw new InvalidStateForOperationException("Cannot archive reviewer");
+        }
+
+        this.state = State.ARCHIVED;
     }
 }
